e5af182acf446ad549a8a3858082ee1d4b1b6e7e
amanraizada <35191629+amanraizada@users.noreply.github.com>
2018-01-18 14:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ def OpenWeb(u):
     chrome_options.binary_location = r"/app/.apt/usr/bin/google-chrome"
     driver = webdriver.Chrome(chrome_options=chrome_options)
     # driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_options)
-    driver.get(u)
     while True:
-        time.sleep(60)
-        print("Refreshing " + u)
-        driver.get(u)
+        try:
+            driver.get(u)
+            time.sleep(60)
+            print("Refreshing " + u)
+        except:
+            continue
+        
 
 url = requests.get("http://textuploader.com/dmchc/raw")
 urls = url.text
