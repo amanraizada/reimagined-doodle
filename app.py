5739929b7b765539169e7212ae9e21981f5ac4cb
amanraizada <35191629+amanraizada@users.noreply.github.com>
2018-01-18 13:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import time,sys,requests,threading
-
+print("STARTING")
 def OpenWeb(u):
     print(u)
     chrome_options = Options()  
@@ -14,7 +14,7 @@ def OpenWeb(u):
     # driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_options)
     driver.get(u)
     while True:
-        time.sleep(10)
+        time.sleep(60)
         print("Refreshing " + u)
         driver.get(u)
 
