fef431237da11e01edb9e020f7777d94db3c8f73
amanraizada <35191629+amanraizada@users.noreply.github.com>
2018-01-18 14:59:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import time,sys,requests,threading
 print("STARTING")
-def OpenWeb(u):
-    print(u)
-    while True:
-        try:
-            chrome_options = Options()  
-            chrome_options.binary_location = r"/app/.apt/usr/bin/google-chrome"
-            driver = webdriver.Chrome(chrome_options=chrome_options)
-            break
-        except:
-            continue
-    # driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_options)
-    while True:
-        try:
-            driver.get(u)
-            time.sleep(60)
-            print("Refreshing " + u)
-        except:
-            continue
-        
 
 url = requests.get("http://textuploader.com/dmchc/raw")
 urls = url.text
 
 urls = urls.split("\n")
 
-for i in urls:
-    while True:
-        try:
-            thread = threading.Thread(target=OpenWeb, args=([i]))
-            thread.start()
-            time.sleep(10)
-            break
-        except:
-            continue
 
+chrome_options = Options()  
+chrome_options.binary_location = r"/app/.apt/usr/bin/google-chrome"
+            
+driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=chrome_options)
+
+while True:
+    for i in urls[:2]:
+        while True:
+            try:
+                driver.get(i)
+                # driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL +"t")
+                time.sleep(1)
+                break
+            except:
+                continue
+    time.sleep(60)
